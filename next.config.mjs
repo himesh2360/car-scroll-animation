dce8c4d98de68a67f57e1899b9a58606eede99d6
himesh2360 <himeshs421@gmail.com>
2026-02-28 12:43:33 +0530

    Revert to standard config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/car-scroll-animation',
-    assetPrefix: '/car-scroll-animation/',
-    images: {
-        unoptimized: true,
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
